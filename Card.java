d5497af96b8582bb142b233abe65432fa9f761a9
EkamSheridan1 <ekam@sheridancollege.ca>
2024-08-07 19:49:03 -0400

    Card.java

--- Card.java
+++ Card.java
@@ -1,25 +1,22 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
-package ca.sheridancollege.project;
+public class Card {
+    private String suit;
+    private String value;
 
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
-public abstract class Card {
-    //default modifier for child classes
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
     @Override
-    public abstract String toString();
-
+    public String toString() {
+        return value + " of " + suit;
+    }
 }
