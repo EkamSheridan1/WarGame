2d7ecf691ecb0b330e11d654afa99c2ae0cfb90b
EkamSheridan1 <ekam@sheridancollege.ca>
2024-08-07 19:50:22 -0400

    Player.java

--- Player.java
+++ Player.java
@@ -1,49 +1,49 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
-package ca.sheridancollege.project;
+import java.util.ArrayList;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Player {
+public class Player {
+    private String name;
+    private ArrayList<Card> hand;
 
-    private String name; //the unique name for this player
-
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
     public Player(String name) {
         this.name = name;
+        this.hand = new ArrayList<>();
     }
 
-    /**
-     * @return the player name
-     */
+    public void drawCard(GroupOfCards deck) {
+        Card drawnCard = deck.drawCard();
+        if (drawnCard != null) {
+            hand.add(drawnCard);
+            System.out.println(name + " draws a card: " + drawnCard);
+        }
+    }
+
+    public Card playCard() {
+        if (!hand.isEmpty()) {
+            return hand.remove(hand.size() - 1);
+        }
+        return null;
+    }
+
+    public Card playSpecificCard(String cardDescription) {
+        for (Card card : hand) {
+            if (card.toString().equalsIgnoreCase(cardDescription)) {
+                hand.remove(card);
+                return card;
+            }
+        }
+        return null;
+    }
+
+    public String showHand() {
+        return hand.toString();
+    }
+
+    public int getHandSize() {
+        return hand.size();
+    }
+
     public String getName() {
         return name;
     }
-
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
-
 }
+
